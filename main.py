ea4a0e1bc1c40d7a00efb61bea6033b2718b4273
Ankit Tiwari <ankit.tiwari@group.one>
2025-08-20 16:39:40 +0530

    fastapi integration

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 from rag_generator import ask_llm
@@ -11,7 +10,4 @@ app = FastAPI()
 @app.post("/ask")
 def ask(q: Question):
     answer = ask_llm(q.question)
-    return {
-        "question": f"You asked: {q.question}",
-        "answer": answer
-    }
+    return answer
